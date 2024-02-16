e8b07818f07c28ddc81de3a4c5ae1dbe8f5bc460
Dave Stanke <davidstanke@gmail.com>
2024-02-16 08:10:20 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Question from "./lib/Question.svelte";
+    import question_texts_raw from "./assets/questions.txt?raw";
+
+    let question_texts = question_texts_raw.split("\n");
+</script>
+
+<header>
+    <a href="https://dora.dev" target="_blank"
+        ><img src="/icon.svg" alt="DORA" /></a
+    >
+</header>
+
+<Question
+    question_text={question_texts[
+        Math.floor(Math.random() * question_texts.length)
+    ]}
+/>
+
+<footer>(TODO)</footer>
+
+<style>
+    header {
+        text-align: center;
+        height: 10vh;
+    }
+
+    header img {
+        width: 50px;
+        padding: 1vw;
+    }
+
+    footer {
+        height: 10vh;
+        text-align: center;
+    }
+</style>
---
+++ src/lib/Question.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let question_text = "<placeholder>";
+
+    let question_text_sentences = [];
+
+    let question_text_escaped = question_text
+        .replaceAll(". ",".<LINEBREAK>")
+        .replaceAll("? ","?<LINEBREAK>")
+
+    question_text_escaped.split("<LINEBREAK>").forEach((sentence) => {
+        question_text_sentences.push(sentence)
+    })
+
+</script>
+
+<section>
+    {#each question_text_sentences as sentence}
+        <p>{sentence}</p>
+    {/each}
+</section>
+
+<style>
+    section {
+        box-sizing: border-box;
+        width: 100vw;
+        height: 80vh;
+        text-align: center;
+        padding: 1vw 5vw;
+        font-size: 5vw;
+    }
+
+    section p {
+        margin:2vw 0;
+        text-shadow: var(--dora-dark-blue) 3px 3px;
+    }
+</style>
